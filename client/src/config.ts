3f14af328532b7d26ef9b142e795091e3eb18f79
haile21 <hailishmit@gmail.com>
2022-10-12 17:36:39 +0300

    some variables assigned values

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,7 +5,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-ovwchvz2.us.auth0.com',            // Auth0 domain :Haile
+  clientId: 'fPEY0YSKolQ4vzJd4uh5cdJhEq6i81D7',          // Auth0 client id: Haile
   callbackUrl: 'http://localhost:3000/callback'
 }
